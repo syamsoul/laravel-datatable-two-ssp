a4908394c9d9d98b4e14584f729ce7256c310057
Syamsoul Azrien <syamsoul.azrien@srglobal.com>
2022-02-13 10:28:25 +0800

    new - filter/search module
    improve
    - csv file affected by the order by
    - re-organize code

--- src/SSP.php
+++ src/SSP.php
@@ -3,6 +3,7 @@ namespace SoulDoit\DataTableTwo;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\DB;
 
 trait SSP{
 	/*
@@ -14,6 +15,7 @@ trait SSP{
 	| Website   : https://github.com/syamsoulcc
 	|
 	*/
+	private $is_search_enable = false;
 	private $arranged_cols_details;
 
 
@@ -100,24 +102,13 @@ trait SSP{
 
 			$the_query_count = $this->dtGetCount($the_query);
 
-			if($frontend_framework == "datatablejs"){
+			$the_query = $this->queryOrder($the_query);
+			$the_query = $this->queryPagination($the_query);
+			$the_query = $this->querySearch($the_query);
 
-				$the_query->orderBy($db_cols_mid[$request->order[0]["column"]], $request->order[0]['dir']);
-
-				if($request->length != "-1") $the_query->limit($request->length)->offset($request->start);
-
-			}elseif(in_array($frontend_framework, ["vuetify", "others"])){
-
-				if($request->filled('sortBy') && $request->filled('sortDesc')){
-					$the_query->orderBy($db_cols_mid[array_flip($db_cols_final)[$request->sortBy]], ($request->sortDesc == 'true' ? 'desc':'asc'));
-				}
-
-				if($request->itemsPerPage != "-1") $the_query->limit($request->itemsPerPage)->offset(($request->page - 1) * $request->itemsPerPage);
-
-			}
+			$the_query_filtered_count = $this->dtGetCount($the_query);
 
 			$the_query_data = $this->getFormattedData($the_query);
-
 
 			if($frontend_framework == "datatablejs"){
 
@@ -139,13 +130,14 @@ trait SSP{
 				$ret['draw'] = $request->draw ?? 0;
 				$ret['data'] = $new_query_data;
 				$ret['recordsTotal'] = $the_query_count;
-				$ret['recordsFiltered'] = $the_query_count; // NOTE: currently filter not functioning yet
+				$ret['recordsFiltered'] = $the_query_filtered_count;
 
 			}elseif(in_array($frontend_framework, ["vuetify", "others"])){
 
 				$ret['data'] = [
 					'items' => $the_query_data,
 					'total_item_count' => $the_query_count,
+					'total_filtered_item_count' => $the_query_filtered_count,
 				];
 
 			}
@@ -184,16 +176,21 @@ trait SSP{
 			'Pragma'              => 'public'
 		];
 
-		$list = $this->getFormattedData($this->dtQuery($this->getArrangedColsDetails()['db_cols']), true);
+		$the_query = $this->dtQuery($this->getArrangedColsDetails()['db_cols']);
+
+		$the_query = $this->queryOrder($the_query);
+		$the_query = $this->querySearch($the_query);
+
+		$the_query_data = $this->getFormattedData($the_query, true);
 
 		// add headers for each column in the CSV download
 		$dt_cols = $this->dtColumns();
 		foreach($dt_cols as $index=>$e_dt_col) if(isset($e_dt_col['is_include_in_doc'])) if(!$e_dt_col['is_include_in_doc']) unset($dt_cols[$index]);
-		array_unshift($list, collect($dt_cols)->pluck('label')->toArray());
+		array_unshift($the_query_data, collect($dt_cols)->pluck('label')->toArray());
 
-		$callback = function() use ($list){
+		$callback = function() use ($the_query_data){
 			$file = fopen('php://output', 'w');
-			foreach ($list as $row) fputcsv($file, $row);
+			foreach ($the_query_data as $row) fputcsv($file, $row);
 			fclose($file);
 		};
 
@@ -222,7 +219,7 @@ trait SSP{
 
 		$dt_cols = $this->dtColumns();
 
-		$db_cols = []; $db_cols_mid = []; $db_cols_final = []; $db_cols_fake = []; $formatter = [];
+		$db_cols = []; $db_cols_initial = []; $db_cols_mid = []; $db_cols_final = []; $db_cols_fake = []; $formatter = [];
 		foreach($dt_cols as $key=>$dt_col){
 			if($is_for_doc) if(isset($dt_col['is_include_in_doc'])) if(!$dt_col['is_include_in_doc']) continue;
 			if(isset($dt_col['db'])){
@@ -231,12 +228,14 @@ trait SSP{
 				if(count($dt_col_db_arr) == 2){
 					$db_cols_final[$key] = $dt_col_db_arr[1];
 					$db_cols_mid[$key] = $dt_col_db_arr[1];
+					$db_cols_initial[$key] = $dt_col_db_arr[0];
 				}else{
 					$dt_col_db_arr = explode(".", $dt_col['db']);
 					if(count($dt_col_db_arr) == 2) $db_cols_final[$key] = $dt_col_db_arr[1];
 					else $db_cols_final[$key] = $dt_col['db'];
 
 					$db_cols_mid[$key] = $dt_col['db'];
+					$db_cols_initial[$key] = $dt_col['db'];
 				}
 			}elseif(isset($dt_col['db_fake'])) $db_cols_fake[$key] = $dt_col['db_fake'];
 
@@ -249,6 +248,7 @@ trait SSP{
 		$arranged_cols_details = [
 			'dt_cols' => $dt_cols,
 			'db_cols' => $db_cols,
+			'db_cols_initial' => $db_cols_initial,
 			'db_cols_mid' => $db_cols_mid,
 			'db_cols_final' => $db_cols_final,
 			'db_cols_fake' => $db_cols_fake,
@@ -258,6 +258,99 @@ trait SSP{
 		if(!$is_for_doc) $this->arranged_cols_details = $arranged_cols_details;
 
 		return $arranged_cols_details;
+	}
+
+
+	private function queryOrder($the_query)
+	{
+		$request = request();
+
+		$frontend_framework = config('sd-datatable-two-ssp.frontend_framework');
+
+		$arranged_cols_details = $this->getArrangedColsDetails();
+		$db_cols_mid = $arranged_cols_details['db_cols_mid'];
+		$db_cols_final = $arranged_cols_details['db_cols_final'];
+
+		if($frontend_framework == "datatablejs"){
+
+			if($request->filled('order')){
+				$the_query->orderBy($db_cols_mid[$request->order[0]["column"]], $request->order[0]['dir']);
+			}
+
+		}elseif(in_array($frontend_framework, ["vuetify", "others"])){
+
+			if($request->filled('sortBy') && $request->filled('sortDesc')){
+				$the_query->orderBy($db_cols_mid[array_flip($db_cols_final)[$request->sortBy]], ($request->sortDesc == 'true' ? 'desc':'asc'));
+			}
+
+		}
+
+		return $the_query;
+	}
+
+
+	private function queryPagination($the_query)
+	{
+		$request = request();
+
+		$frontend_framework = config('sd-datatable-two-ssp.frontend_framework');
+
+		if($frontend_framework == "datatablejs"){
+
+			if($request->filled('length') && $request->filled('start')){
+				if($request->length != "-1") $the_query->limit($request->length)->offset($request->start);
+			}
+
+		}elseif(in_array($frontend_framework, ["vuetify", "others"])){
+
+			if($request->filled('itemsPerPage') && $request->filled('page')){
+				if($request->itemsPerPage != "-1") $the_query->limit($request->itemsPerPage)->offset(($request->page - 1) * $request->itemsPerPage);
+			}
+
+		}
+
+		return $the_query;
+	}
+
+
+	private function querySearch($the_query)
+	{
+		if(!$this->is_search_enable) return $the_query;
+
+		$request = request();
+		$frontend_framework = config('sd-datatable-two-ssp.frontend_framework');
+
+		$search_value = '';
+
+		if($frontend_framework == "datatablejs"){
+
+			if($request->filled('search')){
+				$search_value = $request->search['value'];
+			}
+
+		}elseif(in_array($frontend_framework, ["vuetify", "others"])){
+
+			if($request->filled('search')){
+				$search_value = $request->search;
+			}
+
+		}
+
+		if(!empty($search_value)){
+			$arranged_cols_details = $this->getArrangedColsDetails();
+			$db_cols_initial = $arranged_cols_details['db_cols_initial'];
+			$db_cols_mid = $arranged_cols_details['db_cols_mid'];
+			$db_cols_final = $arranged_cols_details['db_cols_final'];
+
+			$the_query = $the_query->where(function($query) use($db_cols_initial, $search_value){
+				foreach($db_cols_initial as $index=>$e_col){
+					if($index == 0) $query->where($e_col, 'LIKE', "%".$search_value."%");
+					else $query->orWhere($e_col, 'LIKE', "%".$search_value."%");
+				}
+			});
+		}
+
+		return $the_query;
 	}
 
 
