2216d9b2c62bd6797e8a00b90521e11b99f7f8b3
Syamsoul Azrien <syamsoul.azrien@srglobal.com>
2022-07-15 16:26:05 +0800

    change default frontend_framework

--- config/sd-datatable-two-ssp.php
+++ config/sd-datatable-two-ssp.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'frontend_framework' => 'datatablejs', // NOTE: available options = datatablejs, vuetify
+    'frontend_framework' => 'others', // NOTE: available options = datatablejs, vuetify, others
     'export_to_csv' => [
         'is_cache_lock_based_on_auth' => true,
     ],
