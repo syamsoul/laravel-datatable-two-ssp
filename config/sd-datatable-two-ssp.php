1072f4ae47f94e26e1ea8be8b2017fb339a9715b
Syamsoul Azrien <syamsoul.azrien@srglobal.com>
2022-11-25 19:39:21 +0800

    datetime modifier module

--- config/sd-datatable-two-ssp.php
+++ config/sd-datatable-two-ssp.php
@@ -4,4 +4,5 @@ return [
     'export_to_csv' => [
         'is_cache_lock_based_on_auth' => true,
     ],
+    'default_modifier_timezone' => 'UTC', // NOTE: used in DateTimeModifier
 ];
---
+++ src/DateTimeModifier.php
@@ -0,0 +1,35 @@
+<?php
+namespace SoulDoit\DataTableTwo;
+
+use Illuminate\Support\Facades\DB;
+
+class DateTimeModifier{
+    private static $is_constructed=false;
+    private static $timezone;
+
+    public static function construct()
+    {
+        if(!self::$is_constructed){
+            self::$timezone = config('sd-datatable-two-ssp.default_modifier_timezone');
+            self::$is_constructed = true;
+        }
+    }
+
+    public static function getDateTimeCarbon($datetime)
+    {
+        if($datetime instanceof \Carbon\Carbon) return $datetime->copy()->tz(self::$timezone);
+        else return \Carbon\Carbon::parse($datetime)->copy()->tz(self::$timezone);
+        
+        return null;
+    }
+    
+    public static function getMysqlQueryTzRaw($value, $timezone_from=null)
+    {
+        if($timezone_from) $from_datetime_carbon = now($timezone_from);
+        else $from_datetime_carbon = now();
+
+        return DB::raw("CONVERT_TZ($value, '".$timezone_from->format("P")."', '".now(self::$timezone)->format("P")."')");
+    }
+}
+
+DateTimeModifier::construct();
